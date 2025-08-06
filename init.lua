e672d98be32ee97d72408e02e6acfa6e1bbe4960
Pavel <p.a.merkulov@cit.krasnodar.ru>
2025-08-06 11:12:30 +0300

    new config

--- init.lua
+++ init.lua
@@ -1,9 +1,19 @@
-vim.cmd("set number")
+require("config")
 
-require("reknit.remap")
-require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = { "omnisharp" },
-}
+-- Основные настройки
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
-require("lspconfig").omnisharp.setup {}
+-- Номера строк
+vim.opt.number = true
+
+-- Подсветка синтаксиса
+vim.cmd("syntax on")
+
+-- Дополнительные улучшения
+vim.opt.termguicolors = true  -- True Color поддержка
+vim.opt.mouse = "a"           -- Включение мыши
+vim.opt.hidden = true         -- Разрешить скрытые буферы
